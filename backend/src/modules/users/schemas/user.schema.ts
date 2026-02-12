057175c517d102ce90e72394da7ec27a7c100076
miri-cohen20 <miri.devops@gmail.com>
2026-02-12 23:46:09 +0200

    fix bug with insert phone

--- backend/src/modules/users/schemas/user.schema.ts
+++ backend/src/modules/users/schemas/user.schema.ts
@@ -17,7 +17,7 @@ export class User {
   @Prop({ required: true })
   name!: string;
 
-  @Prop({ unique: true, sparse: true })
+  @Prop({ required: false })
   phone?: string;
 
   @Prop({ required: true, unique: true })
